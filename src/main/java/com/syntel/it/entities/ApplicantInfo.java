044cd7562c01b55e97e135b90aa08212bd712cdf
deepti.kotnala@gmail.com <deepti.kotnala@gmail.com>
2018-09-30 20:21:01 -0400

    Last 10 applications

---
+++ src/main/java/com/syntel/it/entities/ApplicantInfo.java
@@ -0,0 +1,36 @@
+package com.syntel.it.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApplicantInfo {
+
+    @Id
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "POSITION")
+    private String position;
+
+    @Column(name = "EMAIL_ID")
+    private String emailId;
+
+    @Column(name = "PHONE_NO")
+    private String phoneNo;
+
+}
